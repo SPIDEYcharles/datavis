4353dbc0da1b06821a208304634dde011497dfc8
SPIDEYcharles <pritham30charles@gmail.com>
2023-09-10 15:50:57 +0530

    commiting a way better optimised but yet to be improved

--- index.js
+++ index.js
@@ -7,24 +7,23 @@ let input= document.getElementById("foi")
 
 let btn= document.getElementById("btn")
 
-function length(){
-    
-    document.getElementById("l").innerText=`${input.value} meter = ${input.value*3.281.toFixed(2)} feet`
-}
-function volume(){
-    
-    document.getElementById("v").innerText=`${input.value} liter = ${input.value*0.264.toFixed(2)} gallon`
-}
-function mass(){
-    
-document.getElementById("m").innerText=`${input.value} kilogram = ${input.value*2.204.toFixed(2)} pound`
-}
-
+   
 btn.addEventListener("click",function(){
+     let basevalue=input.value
+    if(basevalue==""){
+       let p= prompt("Enter a valid number","10")
+       basevalue=p
+      input.setAttribute("value", basevalue)
+    }
+    if(basevalue!=""){
+        basevalue=p
+        input.setAttribute("value", basevalue)
     
-    length()
-    volume()
-    mass()
-    
-    
+    document.getElementById("l").innerText=`${basevalue} meter = ${basevalue*3.281.toFixed(2)} feet`
+
+    document.getElementById("v").innerText=`${basevalue} liter = ${basevalue*0.264.toFixed(2)} gallon`
+
+    document.getElementById("m").innerText=`${basevalue} kilogram = ${basevalue*2.204.toFixed(2)} pound`
+
+     }
 })
